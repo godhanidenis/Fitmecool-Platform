3f2616d12fa346165b128161426b0152c0b99865
denisgodhani <godhanidenis@gmail.com>
2023-03-25 11:29:37 +0530

    resolve floating header and sub geader also resolve product type bug

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 const appConfig = {
-  appUrl: "http://192.168.1.17:8080/graphql",
-  // appUrl:
-  //   "http://rentalplatformbackend-env.eba-tdi32umc.ap-southeast-1.elasticbeanstalk.com/graphql",
+  // appUrl: "http://192.168.1.17:8080/graphql",
+  appUrl:
+    "http://rentalplatformbackend-env.eba-tdi32umc.ap-southeast-1.elasticbeanstalk.com/graphql",
 };
 
 export default appConfig;
