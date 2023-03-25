b0249671c55aa0f25d2aad19252546a9704318ef
godhanidenis <godhanidenis@gmail.com>
2023-03-25 15:49:40 +0530

    reDesign wishlist page

--- pages/productLike/index.js
+++ pages/productLike/index.js
@@ -3,12 +3,11 @@ import { useSelector } from "react-redux";
 import Image from "next/image";
 import Link from "next/link";
 import ProductCard from "../../components/sections/product-section/ProductCard";
-import EmptyCart from "../../assets/images/empty-cart.png";
+import EmptyCart from "../../assets/images/empty_page_img.svg";
+import { Button } from "@mui/material";
 
 const ProductLikePage = () => {
-  const { userProfile, isAuthenticate } = useSelector(
-    (state) => state.userProfile
-  );
+  const { userProfile, isAuthenticate } = useSelector((state) => state.userProfile);
   return (
     <>
       {userProfile.product_like_list?.length === 0 || !isAuthenticate ? (
@@ -20,20 +19,24 @@ const ProductLikePage = () => {
             alt="EmptyCart"
             className="animate__animated animate__slideInDown"
           />
-          <h1 className=" font-bold animate__animated animate__slideInUp">
-            You have not like any product!!
-            <Link href="/" passHref>
+          <h1 className="font-bold animate__animated animate__slideInUp text-3xl">
+            Your wishlist is empty!!
+            {/* <Link href="/" passHref>
               <strong className="text-colorPrimary !font-extrabold underline ml-2 cursor-pointer">
                 View Products
               </strong>
-            </Link>
+            </Link> */}
           </h1>
+          <p>
+            Save your favourite items so you <br /> don't lose sight of them.
+          </p>
+          <button className="text-colorWhite text-base px-4 py-2 w-60 bg-black rounded-md  whitespace-nowrap">
+            Explore Now
+          </button>
         </div>
       ) : (
         <div className="bg-[#F5F5F5] p-5 w-[95%] mx-auto my-10">
-          <p className="text-colorBlack font-semibold text-xl">
-            Liked Products
-          </p>
+          <p className="text-colorBlack font-semibold text-xl">Liked Products</p>
 
           <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10 place-items-center mb-10">
             {userProfile.product_like_list &&
