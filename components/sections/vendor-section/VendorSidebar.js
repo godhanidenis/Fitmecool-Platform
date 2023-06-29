2581cee17daae4c75e8bf44abfe9343d837832d1
denis godhani <godhanidenis@gmail.com>
2023-06-29 10:42:04 +0530

    tooltip add

--- components/sections/vendor-section/VendorSidebar.js
+++ components/sections/vendor-section/VendorSidebar.js
@@ -1,46 +1,60 @@
 import React, { useEffect, useState } from "react";
-import ListAltIcon from "@mui/icons-material/ListAlt";
-import DashboardIcon from "@mui/icons-material/Dashboard";
-import Inventory2Icon from "@mui/icons-material/Inventory2";
-import { useRouter } from "next/router";
-import { Avatar, Divider } from "@mui/material";
-import { useSelector } from "react-redux";
+import { Tooltip, styled, tooltipClasses } from "@mui/material";
 import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 import Rating from "@mui/material/Rating";
 import Box from "@mui/material/Box";
 import StarIcon from "@mui/icons-material/Star";
 import { TbShare3 } from "react-icons/tb";
+import facebookIcon from "../../../assets/facebook.png";
+import googleIcon from "../../../assets/googleIcon.svg";
+import {
+  EmailShareButton,
+  FacebookShareButton,
+  WhatsappIcon,
+  WhatsappShareButton,
+} from "react-share";
+import Image from "next/image";
 
-const VendorSidebar = ({ vendorShopDetails, handleMobileSidebarClick }) => {
-  const router = useRouter();
+const VendorSidebar = ({ vendorShopDetails }) => {
+  const [ratingValue, setRatingValue] = useState(null);
 
-  const { themeLayout } = useSelector((state) => state.themeLayout);
-  const [value, setValue] = useState(vendorShopDetails?.shop_rating);
-  const labels = {
-    1: "1.0",
-    1.5: "1.5",
-    2: "2.0",
-    2.5: "2.5",
-    3: "3.0",
-    3.5: "3.5",
-    4: "4.0",
-    4.5: "4.5",
-    5: "5.0",
-  };
-  function getLabelText(value) {
-    return `${value} Star${value !== 1 ? "s" : ""}, ${labels[value]}`;
-  }
-  const setActiveLink = (path) => {
-    const withoutLastChunk = router.pathname.slice(
-      0,
-      router.pathname.lastIndexOf("/")
+  const [OpenToolTip, setOpenToolTip] = useState(false);
+  const pageShareURL = typeof window !== "undefined" && window.location.href;
+
+  const HtmlTooltip = styled(({ className, ...props }) => (
+    <Tooltip open={OpenToolTip} {...props} classes={{ popper: className }} />
+  ))(({ theme }) => ({
+    [`& .${tooltipClasses.tooltip}`]: {
+      backgroundColor: "#ffffff",
+      color: "rgba(0, 0, 0, 0.87)",
+      maxWidth: 220,
+      fontSize: theme.typography.pxToRem(12),
+      boxShadow: "0 0 10px rgba(0,0,0,.1)",
+    },
+  }));
+
+  const ShareIconComponent = React.forwardRef(function MyComponent(props, ref) {
+    //  Spread the props to the underlying DOM element.
+    return (
+      <div {...props} ref={ref}>
+        <TbShare3
+          onClick={props.onClick}
+          className="lg:text-2xl xl:text-3xl sm:text-xl text-2xl"
+        />
+      </div>
     );
-    return router.pathname === path || withoutLastChunk === path
-      ? "!text-colorPrimary hover:!text-colorPrimary"
-      : "text-[#544E5D] hover:opacity-50";
+  });
+
+  const handleTooltipOpen = () => {
+    setOpenToolTip(!OpenToolTip);
   };
 
-  console.log("data", vendorShopDetails);
+  useEffect(() => {
+    if (vendorShopDetails) {
+      setRatingValue(vendorShopDetails?.shop_rating);
+    }
+  }, [vendorShopDetails]);
+
   return (
     <div className="bg-white rounded-3xl lg:p-10 p-5 py-10">
       <div className="flex justify-center">
@@ -89,8 +103,7 @@ const VendorSidebar = ({ vendorShopDetails, handleMobileSidebarClick }) => {
             }}
             precision={0.5}
             name="read-only"
-            value={value}
-            getLabelText={getLabelText}
+            value={ratingValue}
             emptyIcon={
               <StarIcon
                 sx={{
@@ -107,7 +120,7 @@ const VendorSidebar = ({ vendorShopDetails, handleMobileSidebarClick }) => {
             }
             readOnly
           />
-          {value !== null && (
+          {vendorShopDetails?.shop_rating !== null && (
             <Box
               sx={{
                 ml: 1,
@@ -120,13 +133,53 @@ const VendorSidebar = ({ vendorShopDetails, handleMobileSidebarClick }) => {
                 },
               }}
             >
-              {labels[value]}
+              {ratingValue}
             </Box>
           )}
         </span>
-        <span className="text-2xl text-gray-400 m-10 border border-gray-200 rounded-full p-3">
-          <TbShare3 className="lg:text-2xl xl:text-3xl sm:text-xl text-2xl" />
-        </span>
+        <div
+          onMouseLeave={() => setOpenToolTip(false)}
+          className="text-gray-400 m-10 border border-gray-200 rounded-full p-3 cursor-pointer"
+        >
+          <HtmlTooltip
+            title={
+              // <React.Fragment>
+              <div className="flex">
+                <div className="p-2 rounded-lg cursor-pointer">
+                  <FacebookShareButton
+                    windowWidth={900}
+                    windowHeight={900}
+                    url={pageShareURL}
+                  >
+                    <Image src={facebookIcon ?? ""} alt="facebookIcon" />
+                  </FacebookShareButton>
+                </div>
+                <div className="p-2 rounded-lg cursor-pointer">
+                  <WhatsappShareButton
+                    windowWidth={900}
+                    windowHeight={900}
+                    url={pageShareURL}
+                  >
+                    <WhatsappIcon size={25} round={true} />
+                  </WhatsappShareButton>
+                </div>
+                <div className="p-2 mt-[2px] rounded-lg cursor-pointer">
+                  <EmailShareButton
+                    subject="Product Detail Page"
+                    windowWidth={900}
+                    windowHeight={900}
+                    url={pageShareURL}
+                  >
+                    <Image src={googleIcon ?? ""} alt="googleIcon" />
+                  </EmailShareButton>
+                </div>
+              </div>
+              // </React.Fragment>
+            }
+          >
+            <ShareIconComponent onClick={handleTooltipOpen} />
+          </HtmlTooltip>
+        </div>
       </div>
     </div>
   );
