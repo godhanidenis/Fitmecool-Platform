cfe222d04c85b5ccaa68531daaba0b1ee24eb885
denisgodhani <godhanidenis@gmail.com>
2023-08-28 14:11:15 +0530

    change in config file regarding backend url

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const appConfig = {
-  appUrl: "http://192.168.1.13:8080/graphql",
-  // appUrl: "https://api.rentbless.com/graphql",
+  // appUrl: "http://192.168.1.13:8080/graphql",
+  appUrl: "https://api.rentbless.com/graphql",
 };
 
 export default appConfig;
