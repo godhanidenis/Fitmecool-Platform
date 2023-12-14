4bf3fbb6d8980f8890ca74e948aa8e78f1a3f0aa
denisgodhani <godhanidenis@gmail.com>
2023-12-14 17:56:03 +0530

    fixing sharp installation

---
+++ scripts/install-sharp.js
@@ -0,0 +1,24 @@
+const { execSync } = require("child_process");
+const os = require("os");
+
+// Check the operating system
+const isWindows = os.platform() === "win32";
+console.log("isWindows :", isWindows);
+
+// Define the dependencies based on the operating system
+const dependencies = isWindows
+  ? ["@img/sharp-win32-x64"]
+  : ["@img/sharp-linux-x64"];
+
+// Install the appropriate dependency
+dependencies.forEach((dependency) => {
+  try {
+    console.log("Installing :-", `npm install --force ${dependency} --no-save`);
+    execSync(`npm install --force ${dependency}  --no-save`);
+  } catch (error) {
+    console.error(`Failed to install ${dependency}: ${error.message}`);
+    process.exit(1);
+  }
+});
+
+console.log("Dependencies installed successfully.");
