cf2fac3bb4523844c0d9f01023f0e182804a546f
denisgodhani <godhanidenis@gmail.com>
2023-12-15 12:19:06 +0530

    force install

--- scripts/install-sharp.js
+++ scripts/install-sharp.js
@@ -8,13 +8,13 @@ console.log("isWindows :", isWindows);
 // Define the dependencies based on the operating system
 const dependencies = isWindows
   ? ["@img/sharp-win32-x64"]
-  : ["--os=linux --cpu=x64 sharp"];
+  : ["@img/sharp-linux-x64"];
 
 // Install the appropriate dependency
 dependencies.forEach((dependency) => {
   try {
-    console.log("Installing :-", `npm install ${dependency}`);
-    execSync(`npm install ${dependency}`);
+    console.log("Installing :-", `npm install --force ${dependency} --no-save`);
+    execSync(`npm install --force ${dependency} --no-save`);
   } catch (error) {
     console.error(`Failed to install ${dependency}: ${error.message}`);
     process.exit(1);
